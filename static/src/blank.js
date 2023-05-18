c24a0c56642c96454c452bdf59b6d61948c457cd
Daniel <danielmr@uninorte.edu.co>
2023-05-18 00:13:12 -0500

    Tabla completa, filtros medianamente funcionales

--- static/src/blank.js
+++ static/src/blank.js
@@ -64,6 +64,74 @@ $(document).ready(function () {
         return opciones;
       }
 
+      // Resto del código de paginación de la tabla...
+
+      // Función para filtrar la tabla
+      function filtrarTabla() {
+        var filtroSeleccionado = {};
+
+        $(".filtro").each(function () {
+          var columna = $(this).data("columna");
+          var valorFiltro = $(this).val();
+
+          if (valorFiltro) {
+            filtroSeleccionado[columna] = valorFiltro;
+          }
+        });
+
+        var paginaDestino = encontrarPaginaFiltro(filtroSeleccionado);
+
+        if (paginaDestino > 0) {
+          currentPage = paginaDestino;
+          showPage(currentPage);
+        }
+      }
+
+      // Función para encontrar la página que contiene el filtro seleccionado
+      function encontrarPaginaFiltro(filtro) {
+        for (var i = 0; i < totalPages; i++) {
+          var startIndex = i * 10;
+          var endIndex = startIndex + 10;
+          var pageRows = rows.slice(startIndex, endIndex);
+
+          for (var j = 0; j < pageRows.length; j++) {
+            var cols = pageRows[j].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+            var coincidencia = true;
+
+            for (var prop in filtro) {
+              var columna = parseInt(prop);
+              var valorFiltro = filtro[prop];
+              var valorCelda = cols[columna].trim().replace(/"/g, "");
+
+              if (valorFiltro !== valorCelda) {
+                coincidencia = false;
+                break;
+              }
+            }
+
+            if (coincidencia) {
+              return i + 1;
+            }
+          }
+        }
+
+        return 0; // No se encontró ninguna coincidencia
+      }
+
+      // Función para ajustar el tamaño de la tabla
+      function adjustTableSize() {
+        const rows = $("#tabla-aves tbody tr");
+
+        rows.each(function () {
+          const row = $(this);
+          const cells = row.find("td");
+
+          cells.css({ width: "auto", height: "auto" });
+          row.css("height", row.prop("scrollHeight") + "px");
+        });
+      }
+
+      // Función para mostrar una página específica
       function showPage(page) {
         var startIndex = (page - 1) * 10;
         const endIndex = startIndex + 10;
@@ -123,103 +191,9 @@ $(document).ready(function () {
         actualizarPaginacion(page);
       }
 
-      // Función para filtrar la tabla
-      function filtrarTabla() {
-        var filtroSeleccionado = {};
-
-        $(".filtro").each(function () {
-          var columna = $(this).data("columna");
-          var valorFiltro = $(this).val();
-
-          if (valorFiltro) {
-            filtroSeleccionado[columna] = valorFiltro;
-          }
-        });
-
-        var paginaDestino = encontrarPaginaFiltro(filtroSeleccionado);
-
-        if (paginaDestino > 0) {
-          currentPage = paginaDestino;
-          showPage(currentPage);
-        }
-      }
-
-      // Función para encontrar la página que contiene el filtro seleccionado
-      function encontrarPaginaFiltro(filtro) {
-        for (var i = 0; i < totalPages; i++) {
-          var startIndex = i * 10;
-          var endIndex = startIndex + 10;
-          var pageRows = rows.slice(startIndex, endIndex);
-
-          for (var j = 0; j < pageRows.length; j++) {
-            var cols = pageRows[j].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-
-            var coincideFiltro = Object.keys(filtro).every(function (columna) {
-              var valorFiltro = filtro[columna];
-              var valorCelda = cols[columna].trim().replace(/"/g, "");
-              return valorCelda === valorFiltro;
-            });
-
-            if (coincideFiltro) {
-              return i + 1;
-            }
-          }
-        }
-
-        return -1;
-      }
-
-      // Función para cargar imágenes desde los enlaces de la última columna
-      function cargarImagenes() {
-        const filas = document.querySelectorAll("#tabla-aves tbody tr");
-
-        filas.forEach((fila) => {
-          const celdaImagen = fila.querySelector("td:last-child"); // Última columna
-          const enlaceImagen = celdaImagen.textContent;
-
-          if (enlaceImagen) {
-            const imagen = new Image();
-            imagen.src = enlaceImagen;
-            imagen.classList.add("imagen-tabla");
-
-            const enlace = document.createElement("a");
-            enlace.href = enlaceImagen;
-            enlace.target = "_blank";
-            enlace.appendChild(imagen);
-
-            celdaImagen.textContent = ""; // Eliminar el enlace existente
-            celdaImagen.appendChild(enlace);
-          }
-        });
-      }
-
-      // Mostrar la página actual
-      function showPage(page) {
-        var startIndex = (page - 1) * 10;
-        var endIndex = startIndex + 10;
-        var pageRows = rows.slice(startIndex, endIndex);
-
-        $("#tabla-aves tbody").empty();
-
-        pageRows.forEach((row) => {
-          const cols = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-          const tr = document.createElement("tr");
-
-          cols.forEach((col) => {
-            const td = document.createElement("td");
-            td.textContent = col.trim().replace(/"/g, "");
-            tr.appendChild(td);
-          });
-
-          $("#tabla-aves tbody").append(tr);
-        });
-
-        cargarImagenes(); // Cargar las imágenes después de mostrar la página
-      }
-
-      showPage(currentPage); // Mostrar la página actual al cargar la tabla
+      // Resto del código de paginación de la tabla...
     })
     .catch((error) => {
-      console.log("Error al cargar el archivo CSV:", error);
+      console.error("Error al cargar el archivo CSV:", error);
     });
 });
